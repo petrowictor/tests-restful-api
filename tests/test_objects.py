d7b93484cfb71fd03be327392c5824ef976a2675
viktorianen <petrov_v_viktor@mail.ru>
2025-09-13 16:39:25 +0500

    add pytest.ini, test.yml, requirements.txt, add allure to test_objects.py

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,9 +1,12 @@
+import allure
+
 from endpoints.create_object import CreateObject
 from endpoints.get_object import GetObject
 from endpoints.update_object import UpdateObject
 from endpoints.delete_object import DeleteObject
 
 
+@allure.title("Create object")
 def test_create_object():
    new_object_endpoint = CreateObject()
    payload = {
@@ -20,12 +23,14 @@ def test_create_object():
    new_object_endpoint.check_response_is_200()
    new_object_endpoint.check_name(payload["name"])
 
+@allure.title("Get object")
 def test_get_object(obj_id):
    get_obj_endpoint = GetObject()
    get_obj_endpoint.get_by_id(obj_id)
    get_obj_endpoint.check_response_is_200()
    get_obj_endpoint.check_response_id(obj_id)
 
+@allure.title("Update object")
 def test_update_object(obj_id):
    update_obj_endpoint = UpdateObject()
    payload = {
@@ -41,6 +46,7 @@ def test_update_object(obj_id):
    update_obj_endpoint.check_response_is_200()
    update_obj_endpoint.check_response_name(payload['name'])
 
+@allure.title("Delete object")
 def test_delete_object(obj_id):
    delete_obj_endpoint = DeleteObject()
    delete_obj_endpoint.delete_by_id(obj_id)
